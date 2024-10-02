d68f591ef7061c3f73df1ff182eff02f7a435ced
Adetayo <twinebravo@gmail.com>
2024-10-02 05:41:35 +0100

    Updated java file

--- Bravotaskjv/src/task/Checkout.java
+++ Bravotaskjv/src/task/Checkout.java
@@ -8,34 +8,54 @@ public class Checkout {
     public static void main(String[] args) {
         ArrayList<String> checkouts = new ArrayList<>();
 
-        //String collectCustomerName = "Bravo";
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter Customer Name: ");
-        String collectCustomerName = input.nextLine();
-        System.out.println("Enter Product Name: ");
-        String collectProductName = input.nextLine();
-        int collectProductQuantity = input.nextInt();
 
-        String getName = getCustomerName(collectCustomerName);
-        String getProductName = getProductName(collectProductName);
-        int getProductQuantity = getProductQuantity(collectProductQuantity);
+        String moreItems = "";
 
-        checkouts.add(getName);
-        for(String customerName : checkouts) {
-            System.out.println(customerName);
-            System.out.println(getProductName);
-            System.out.println(getProductQuantity);
+        do {
+            System.out.println("Enter Customer Name: ");
+            String collectCustomerName = input.nextLine();
+
+            System.out.println("Enter Product Name: ");
+            String collectProductName = input.nextLine();
+
+            System.out.println("Enter Product Quantity: ");
+            int collectProductQuantity = input.nextInt();
+
+            System.out.println("Enter How Much per Unit: ");
+            double collectProductPrice = input.nextDouble();
+            input.nextLine();
+
+            String getName = getCustomerName(collectCustomerName);
+            String getProductName = getProductName(collectProductName);
+            int getProductQuantity = getProductQuantity(collectProductQuantity);
+            double getProductPrice = getProductPrice(collectProductPrice);
+
+            checkouts.add("Customer Name: " + getName);
+            checkouts.add("Product Name: " + getProductName);
+            checkouts.add("Product Quantity: " + getProductQuantity);
+            checkouts.add("Product Unit Price: " + getProductPrice);
+
+            System.out.println("Add More Items? (yes/no): ");
+            moreItems = input.nextLine();
+        }
+        while(moreItems.equalsIgnoreCase("yes"));
+
+
+        for (String details : checkouts) {
+            System.out.println(details);
         }
     }
+
+
 
     public static String checkout() {
+        ArrayList<String> checkouts = new ArrayList<>();
         return null;
     }
-
     public static String getCustomerName(String collectCustomerName) {
-            return collectCustomerName;
-        }
-
+        return collectCustomerName;
+    }
 
     public static String getProductName(String collectProductName) {
         return collectProductName;
@@ -44,4 +64,12 @@ public class Checkout {
     public static int getProductQuantity(int collectProductQuantity) {
         return collectProductQuantity;
     }
+
+    public static double getProductPrice(double collectProductPrice) {
+        return collectProductPrice;
+    }
+
 }
+
+
+
